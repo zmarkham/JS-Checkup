9ec80431235b6789841225757ca5b01bee328937
Zontrae Markham <zontraemarkham@gmail.com>
2021-09-10 09:46:28 -0400

    Git 00-assignment page added

---
+++ 00-assignment.js
@@ -0,0 +1,24 @@
+function hello() {
+    console.log("Hello World!")
+}
+
+let greeting = "Hi";
+let firstName = "Zontrae";
+let lastName = "Markham";
+
+console.log(`${greeting}! I'm ${firstName} ${lastName}. I just moved to this neighborhood, I purchased the fixer upper to renovate.`);
+
+let cities = ["South Bend", "Indianapolis", "Mishawaka", "Atlanta"]
+console.log(cities[0]);
+
+let paint = "Mint"
+
+if (paint === "Blue"){
+    console.log("I'll paint the bathroom Blue.")
+}else if (paint === "Lavendar"){
+    console.log("I'll paint the bathroom Lavendar.")
+}else if (paint === "Mint"){
+    console.log("I'll paint the bathroom Mint.")
+}else {
+    console.log("I guess I need to visit Lowes")
+}
